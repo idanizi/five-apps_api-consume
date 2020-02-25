24ba6beb1bfeb4be96be7970668c1ba836311e98
Idan Izicovich <idanizi@Idans-MacBook-Pro.local>
2020-02-26 01:44:44 +0200

    Updates

---
+++ precache-manifest.daf007cacc7318f5fe4bf62ab8331653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f3a2cf61e87dd3b6446acc40628d51",
+    "url": "/five-apps_api-consume/index.html"
+  },
+  {
+    "revision": "b084b8f5dc3266f827b5",
+    "url": "/five-apps_api-consume/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "86137d0385d18ad34db9",
+    "url": "/five-apps_api-consume/static/css/main.925056b7.chunk.css"
+  },
+  {
+    "revision": "b084b8f5dc3266f827b5",
+    "url": "/five-apps_api-consume/static/js/2.f8a4677a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/five-apps_api-consume/static/js/2.f8a4677a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86137d0385d18ad34db9",
+    "url": "/five-apps_api-consume/static/js/main.7e3f7153.chunk.js"
+  },
+  {
+    "revision": "16d21d04c0c1a5335053",
+    "url": "/five-apps_api-consume/static/js/runtime-main.4ef1f86b.js"
+  }
+]);
